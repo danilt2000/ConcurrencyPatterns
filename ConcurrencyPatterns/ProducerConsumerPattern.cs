892845f21e6b9ba5146886280365619cc3217118
Hepatica <danil.tkachenko@getmore.cz>
2024-07-28 03:51:39 +0200

    Add project files.

---
+++ ConcurrencyPatterns/ProducerConsumerPattern.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrencyPatterns
+{
+        internal class ProducerConsumerPattern
+        {
+                public ProducerConsumerPattern() { }
+
+                public async Task InvokePattern()
+                {
+                        var buffer = new BlockingCollection<int>(boundedCapacity: 10);
+
+                        var producerTask = Task.Run(() =>
+
+                                {
+                                        for (int i = 0; i < 20; i++)
+                                        {
+                                                buffer.Add(i);
+                                                Console.WriteLine($"Produced:{i}");
+
+                                                Thread.Sleep(100);
+                                        }
+
+                                        buffer.CompleteAdding();
+                                }
+                        );
+                        var consumerTask = Task.Run(() =>
+                        {
+                                foreach (var item in buffer.GetConsumingEnumerable())
+                                {
+                                        Console.WriteLine($"Consumed:{item}");
+                                        Thread.Sleep(200);
+                                }
+
+
+                        });
+                 
+                        await Task.WhenAll(producerTask, consumerTask);
+                }
+        }
+}
---
+++ ConcurrencyPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConcurrencyPatterns;
+
+internal class Program
+{
+        public static async Task Main(string[] args)
+        {
+                ProducerConsumerPattern producerConsumerPattern = new ProducerConsumerPattern();
+
+                await producerConsumerPattern.InvokePattern();
+
+        }
+}
