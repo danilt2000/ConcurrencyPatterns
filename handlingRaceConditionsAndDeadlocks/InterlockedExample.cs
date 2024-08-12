bc3aaeddff2e789dc4df557cca0dcdd25f9a9393
Hepatica <75219332+danilt2000@users.noreply.github.com>
2024-08-12 16:28:34 +0200

    Add files via upload
    Introduce interlocked and monitor classes to handle race conditions and deadlocks

---
+++ handlingRaceConditionsAndDeadlocks/InterlockedExample.cs
@@ -0,0 +1,34 @@
+﻿namespace HandlingRaceConditionsAndDeadlocks
+{
+    internal class InterlockedExample
+    {
+        private static int _counter = 0;
+
+        public void Invoke()
+        {
+            Thread t1 = new Thread(IncrementCounter);
+
+            Thread t2 = new Thread(IncrementCounter);
+
+            t1.Start();
+
+            t2.Start();
+
+            t1.Join();
+
+            t2.Join();
+
+            Console.WriteLine($"Final counter value:{_counter}");
+        }
+
+        static void IncrementCounter()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.WriteLine($"Current index {i}");
+
+                Interlocked.Increment(ref _counter);
+            }
+        }
+    }
+}
---
+++ handlingRaceConditionsAndDeadlocks/TimeoutsResourceAllocation.cs
@@ -0,0 +1,88 @@
+﻿namespace HandlingRaceConditionsAndDeadlocks
+{
+    internal class TimeoutsResourceAllocation
+    {
+        private static readonly object Lock1 = new object();
+
+        private static readonly object Lock2 = new object();
+
+        public void Invoke()
+        {
+            Thread t1 = new Thread(DeadlockMethod1);
+
+            Thread t2 = new Thread(DeadlockMethod2);
+
+            t1.Start();
+
+            t2.Start();
+
+            t1.Join();
+
+            t2.Join();
+
+            Console.WriteLine($"Execution completed");
+        }
+
+        static void DeadlockMethod1()
+        {
+            if (Monitor.TryEnter(Lock1, TimeSpan.FromSeconds(1)))
+            {
+                try
+                {
+                    Console.WriteLine("Thread 1 acquired lock1.");
+
+                    Thread.Sleep(2000);//Simulate some work 
+
+                    if (Monitor.TryEnter(Lock2, TimeSpan.FromSeconds(1)))
+                    {
+                        try
+                        {
+                            Thread.Sleep(2000);//Simulate some work 
+
+                            Console.WriteLine("Thread 1 acquired lock2.");
+                        }
+                        finally
+                        {
+                            Monitor.Exit(Lock2);
+                        }
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(Lock1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Thread 1 couldn't acquire lock1 within timeout.");
+            }
+
+            //lock (Lock1)
+            //{
+            //    Console.WriteLine("Thread 1 acquired lock1.");
+
+            //    Thread.Sleep(1000);
+
+            //    lock (Lock2)
+            //    {
+            //        Console.WriteLine("Thread 1 acquired lock2.");
+            //    }
+            //}
+        }
+
+        static void DeadlockMethod2()
+        {
+            lock (Lock2)
+            {
+                Console.WriteLine("Thread 2 acquired lock2.");
+
+                Thread.Sleep(1000);
+
+                lock (Lock1)
+                {
+                    Console.WriteLine("Thread 2 acquired lock1.");
+                }
+            }
+        }
+    }
+}
